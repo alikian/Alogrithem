5fdcb4a754ad6d5b41f3c7bad931e739e1c81fde
Ali <alikian@gmail.com>
2019-03-09 22:07:38 -0800

    resize

--- python/EightQueen/DrawBoard.py
+++ python/EightQueen/DrawBoard.py
@@ -4,9 +4,12 @@ turtle.speed(-1)
 
 width = 60
 
+xRef = -4 * width
+yRef = -4 * width
+
 
 def draw_squre(x, y):
-    turtle.goto(x, y)
+    turtle.goto(x + xRef, y + yRef)
     turtle.begin_fill()
     turtle.down()  # "Pen" down?
     for i in range(4):  # For each edge of the shape
@@ -17,17 +20,19 @@ def draw_squre(x, y):
 
 
 def draw_board():
+    screen = turtle.getscreen()
+    screen.setup(width=width * 10, height=width * 10)
     for row in range(9):
         turtle.up()  # "Pen" down?
-        turtle.goto(row * width, 0)
+        turtle.goto(row * width + xRef, 0 + yRef)
         turtle.down()  # "Pen" down?
-        turtle.goto(row * width, width * 8)
+        turtle.goto(row * width + xRef, width * 8 + yRef)
         turtle.up()
 
         turtle.up()  # "Pen" down?
-        turtle.goto(0, row * width)
+        turtle.goto(0+xRef, row * width+yRef)
         turtle.down()  # "Pen" down?
-        turtle.goto(width * 8, row * width)
+        turtle.goto(width * 8+xRef, row * width+yRef)
         turtle.up()
 
     for col in range(4):
@@ -37,7 +42,7 @@ def draw_board():
 
 
 def draw_queen(row, col, erase):
-    index = col * 8 + row+col
+    index = col * 8 + row + col
 
     turtle.pencolor('black')
     turtle.pensize(4)
@@ -52,9 +57,9 @@ def draw_queen(row, col, erase):
     turtle.up()
     rad = width / 3
     x = col * width + width / 2
-    y = row * width + width / 2 -rad
+    y = row * width + width / 2 - rad
 
-    turtle.goto(x, y)
+    turtle.goto(x+xRef, y+yRef)
     turtle.down()
     turtle.circle(rad)
     turtle.color('black')
